8b6c096d20b3a2dcf93e365e82e70c8066ae2b61
Jernej Strasner <jernej.strasner@me.com>
2012-01-17 16:55:44 +0100

    Set the organization name

--- JSTwitter/JSViewController.m
+++ JSTwitter/JSViewController.m
@@ -3,7 +3,7 @@
 //  JSTwitter
 //
 //  Created by Jernej Strasner on 1/17/12.
-//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//  Copyright (c) 2012 JernejStrasner.com. All rights reserved.
 //
 
 #import "JSViewController.h"
