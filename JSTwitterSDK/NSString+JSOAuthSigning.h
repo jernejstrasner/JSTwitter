73b7cd7e20e35021d89f48d85901ae6e31085524
Jernej Strasner <jernej.strasner@me.com>
2012-01-19 12:54:32 +0100

    Added a NSString category for generating the request signature

---
+++ JSTwitterSDK/NSString+JSOAuthSigning.h
@@ -0,0 +1,13 @@
+//
+//  NSString+JSOAuthSigning.h
+//  JSTwitter
+//
+//  Created by Jernej Strasner on 1/19/12.
+//  Copyright (c) 2012 JernejStrasner.com. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NSString (JSOAuthSigning)
+
+@end
---
+++ JSTwitterSDK/NSString+JSOAuthSigning.m
@@ -0,0 +1,34 @@
+//
+//  NSString+JSOAuthSigning.m
+//  JSTwitter
+//
+//  Created by Jernej Strasner on 1/19/12.
+//  Copyright (c) 2012 JernejStrasner.com. All rights reserved.
+//
+
+#import "NSString+JSOAuthSigning.h"
+
+#import <CommonCrypto/CommonDigest.h>
+#import <CommonCrypto/CommonHMAC.h>
+
+#import "Base64Transcoder.h"
+
+@implementation NSString (JSOAuthSigning)
+
+- (NSString *)HMACSHA1SignatureWithSecret:(NSString *)secret
+{
+    NSData *stringData = [self dataUsingEncoding:NSUTF8StringEncoding];
+    NSData *secretData = [self dataUsingEncoding:NSUTF8StringEncoding];
+    
+    unsigned char result[CC_SHA1_DIGEST_LENGTH];
+    CCHmac(kCCHmacAlgSHA1, [secretData bytes], [secretData length], [stringData bytes], [stringData length], result);
+    
+    char base64_result[32];
+    size_t base64_result_length = 32;
+    
+    Base64EncodeData(result, CC_SHA1_DIGEST_LENGTH, base64_result, &base64_result_length);
+    
+    return [NSString stringWithFormat:@"%s", base64_result];
+}
+
+@end
